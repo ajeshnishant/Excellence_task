f25168c0306d0a72d43ab4a08ae74b8d034a4104
ajeshnishant <61043063+ajeshnishant@users.noreply.github.com>
2021-05-28 18:10:01 +0530

    Update urls.py

--- candidate/myapp/urls.py
+++ candidate/myapp/urls.py
@@ -1,11 +1,10 @@
 from rest_framework.routers import DefaultRouter
 from .views import CandidateViewSet, TestScoreViewSet, AverageScore , Highest_scorer
-    # AverageScoreViewSet
 from django.urls import include, path
 router = DefaultRouter()
 router.register(r'candidate', CandidateViewSet)
 router.register(r'testscore', TestScoreViewSet)
-# router.register(r'Averagescore', AverageScore)
+
 
 urlpatterns = [
     path('', include(router.urls)),
